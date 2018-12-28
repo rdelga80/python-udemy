311c2c6e9367e655a02e49d6af1a6ca17509f4eb
Ricardo Delgado <ricdelgado80@gmail.com>
2018-12-28 12:42:13 -0500

    more lessons

--- fileio.py
+++ fileio.py
@@ -10,7 +10,43 @@
 
 # EXAMPLE 2
 
+# with open('sample.txt', 'r') as jabber:
+#   for line in jabber:
+#     if "JAB" in line.upper():
+#       print(line, end='')
+
+# EXAMPLE 3
+# to remove extra line
+
+# with open('sample.txt', 'r') as jabber:
+#   line = jabber.readline()
+#   while line:
+#     print(line, end='')
+#     line = jabber.readline()
+
+# EXAMPLE 4
+
+# with open('sample.txt', 'r') as jabber:
+#   lines = jabber.readlines()
+# print(lines)
+
+# for line in lines:
+#   print(line, end='')
+
+# EXAMPLE 5
+
+# with open('sample.txt', 'r') as jabber:
+#   lines = jabber.readlines()
+# print(lines)
+
+# for line in lines[::-1]:
+#   print(line, end='')
+
+# EXAMPLE 6
+
 with open('sample.txt', 'r') as jabber:
-  for line in jabber:
-    if "JAB" in line.upper():
-      print(line, end='')
+  lines = jabber.read()
+print(lines)
+
+for line in lines[::-1]:
+  print(line, end='')
